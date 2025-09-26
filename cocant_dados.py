460c1e06799c95f57928cc74d818b483cc42b2e7
Igor de Araújo <103179508+Ygh0r@users.noreply.github.com>
2025-09-26 20:15:19 -0300

    cocant_dados v0.1.0

---
+++ cocant_dados.py
@@ -0,0 +1,16 @@
+# Desafio 1, Concatenando Dados.
+# Vamos receber dois dados diferentes do usuário e concatená-los em uma única string.
+
+# Constantes para melhor visualização e alteração futura do código.
+MENSAGEM_APRESENTAÇÃO = "Bem-vindo ao programa de concatenação de dados!"
+PRIMEIRA_MENSAGEM = "Insira um valor de sua escolha: "
+SEGUNDA_MENSAGEM = "Insira outro valor: "
+
+# Primeira e segunda entrada do usuário.
+print(MENSAGEM_APRESENTAÇÃO)
+primeiro_valor = str(input(PRIMEIRA_MENSAGEM))
+segundo_valor = str(input(SEGUNDA_MENSAGEM))
+
+palavra_formada = primeiro_valor + " " + segundo_valor
+
+print(palavra_formada)
